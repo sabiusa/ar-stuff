cfede0f06871a8a434d349e46d18b14c42434e64
Saba Khutsishvili <sabakhuts@gmail.com>
2021-04-05 18:47:37 +0400

    Add sphere to the scene

--- Shape/Shape/ViewController.swift
+++ Shape/Shape/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        sceneView.debugOptions = [
+            SCNDebugOptions.showWorldOrigin,
+            SCNDebugOptions.showFeaturePoints
+        ]
         
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +38,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        drawSphereAtOrigin()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -40,5 +47,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func drawSphereAtOrigin() {
+        let geometry = SCNSphere(radius: 0.05)
+        let sphere = SCNNode(geometry: geometry)
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        sphere.position = SCNVector3(x: 0, y: 0, z: 0)
+        
+        sceneView.scene.rootNode.addChildNode(sphere)
     }
 }
