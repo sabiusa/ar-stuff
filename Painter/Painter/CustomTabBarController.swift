8a44d9364475c6a2f92c008a84d33f2d4139777d
Saba Khutsishvili <sabakhuts@gmail.com>
2021-04-06 14:31:40 +0400

    Add initial setup for  app

---
+++ Painter/Painter/CustomTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTabBarController.swift
+//  Painter
+//
+//  Created by Saba Khutsishvili on 4/6/21.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    var brushSettings = BrushSettings()
+
+}
