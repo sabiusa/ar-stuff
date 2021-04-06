ec9ea80c8ecf651b333afa381e052fb5ab9af23b
Saba Khutsishvili <sabakhuts@gmail.com>
2021-04-06 14:31:40 +0400

    Add initial setup for `Painter` app

---
+++ Painter/Painter/BrushSettings.swift
@@ -0,0 +1,27 @@
+//
+//  BrushSettings.swift
+//  Painter
+//
+//  Created by Saba Khutsishvili on 4/6/21.
+//
+
+import Foundation
+import UIKit
+
+class BrushSettings {
+    enum Shape {
+        case box
+        case capsule
+        case cone
+        case cylinder
+        case pyramid
+        case sphere
+        case torus
+        case tube
+    }
+
+    var color = UIColor.orange
+    var shape = Shape.box
+    var size: CGFloat = 0.5
+    var isSpinning = false
+}
