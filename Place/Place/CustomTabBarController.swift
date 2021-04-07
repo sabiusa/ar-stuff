b9a0f120539fb97127944aae32353941522dafd7
Saba Khutsishvili <sabakhuts@gmail.com>
2021-04-07 07:59:20 +0400

    Add initial setup for placing app

---
+++ Place/Place/CustomTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTabBarController.swift
+//  Place
+//
+//  Created by Saba Khutsishvili on 4/7/21.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    var furnitureSettings = FurnitureSettings()
+    
+}
